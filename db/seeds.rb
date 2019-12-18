7e0e4f6bc9a39cfdf6ef6d587da023eb1573757f
yuuma.maki <postafkamen1yu_ai@yahoo.co.jp>
2019-12-18 10:23:33 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Task.create(status: 'T1', content: 'test1')
+# Task.create(status: 'T2', content: 'test2')
+# Task.create(status: 'T3', content: 'test3')
+# Task.create(status: 'T4', content: 'test4')
+
+(1..10).each do |number|
+    Task.create(status: 'test' + number.to_s, content: 'test task' + number.to_s)
+end
