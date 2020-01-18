351e29efb4ce98f2b21bdf5a05153e9681310659
yuuma.maki <postafkamen1yu_ai@yahoo.co.jp>
2020-01-18 07:44:20 +0000

    tasklogin8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -62,7 +62,7 @@ class TasksController < ApplicationController
     end
     
     def correct_user
-     @tasks = current_user.tasks.find_by(id: params[:id])
+     @task = current_user.tasks.find_by(id: params[:id])
      unless @task
         redirect_to root_url
      end
